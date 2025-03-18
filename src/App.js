243825c74cfd69d3c31985692d9ba21e6acc7174
Raghu Nandan Lal Garikipati <53864248+GRaghuNandanLal@users.noreply.github.com>
2025-03-18 17:16:05 -0500

    Listings

--- src/App.js
+++ src/App.js
@@ -4,8 +4,14 @@ import Login from "./components/Login";
 import Home from "./components/Home";
 import Header from "./components/Header";
 import Register from "./components/Register";
+import ClientHome from "./components/ClientHome";
+import WorkerHome from "./components/WorkerHome";
+import ViewWorker from "./components/ViewWorker";
+import ClientService from "./components/ClientService";
+import ClientRequest from "./components/ClientRequest";
 import "bootstrap/dist/css/bootstrap.css";
-import Edit from "./components/edit";
+import ClientMeeting from "./components/ClientMeeting";
+import Rating from "./components/Rating";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "react-bootstrap";
 import "./App.css";
@@ -18,7 +24,13 @@ const App = () => {
         <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/edit" element={<Edit />} />
+        <Route path="/client-home" element={<ClientHome />} />
+        <Route path="/worker-home" element={<WorkerHome />} />
+        <Route path="/view-worker" element={<ViewWorker />} />
+        <Route path="/client-service" element={<ClientService />} />
+        <Route path="/client-request" element={<ClientRequest />} />
+        <Route path="/client-meeting" element={<ClientMeeting />} />
+        <Route path="/rating" element={<Rating />} />
         <Route
           path="/forgot-password"
           element={<div>Forgot Password Page</div>}
